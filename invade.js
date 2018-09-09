52180c72d94208ff8a621193a6c90f372a217ece
David Rowthorn <davidrowthorn@gmail.com>
2018-09-09 16:00:11 +0100

    side-to-side alien

--- invade.js
+++ invade.js
@@ -28,6 +28,9 @@ const shapes = {
     [0, 1, 1, 1, 0],
     [0, 1, 1, 1, 0],
     [1, 1, 1, 1, 1],
+  ],
+  bullet: [
+    [1]
   ]
 }
 
@@ -49,23 +52,67 @@ function addToArena(piece) {
     row.forEach((value, x) => {
       let arenaPos = arena[y + piece.offset.y][x + piece.offset.x]
       if (value !== 0) {
-        arena[y + piece.offset.y][x + piece.offset.x] = piece.matrix[y][x]
+        arena[y + piece.offset.y][x + piece.offset.x] = piece.matrix[y][x] * piece.type
       }
     })
   })
 }
 
+
+
+// Collision logic
+
+function handleCollision (piece, axis, target = 1) {
+  if (piece.type === 1) return
+
+  if (axis === "x") {
+    if (piece.direction.x < 0) {
+      var side = "left"
+    } else {
+      var side = "right"
+    }
+  } else {
+    if (piece.direction.y < 0) {
+      var side = "bottom"
+    } else {
+      var side = "top"
+    }
+  }
+
+  if (piece.type === 2) {
+    piece.destroy = true
+  }
+
+  if (piece.type === 3) {
+    if (side === "bottom") {
+      // Game Over
+    } else if (side === "left" || side === "right") {
+      piece.direction.x = piece.direction.x * -1
+    }
+  }
+}
+
+
 function collides (piece) {
   let [m, o] = [piece.matrix, piece.offset]
   for (let y = 0; y < m.length; ++y) {
     for (let x = 0; x < m[y].length; ++x) {
+      if (!arena[o.y + y]) {
+        handleCollision(piece, 'y', target)
+        return true
+      }
       if (arena[o.y + y][o.x + x] !== 0) {
+        handleCollision(piece, 'x', target)
         return true
       }
     }
   }
   return false
 }
+
+
+
+
 
 function drawMatrix(matrix, offset) {
   matrix.forEach((row, y) => {
@@ -91,48 +138,69 @@ function shoot(piece) {
   let x = piece.offset.x + 2
   let y = piece.offset.y
   let bullet = {
-    offset: {x: x, y: y}
+    type: 2,
+    destroy: false,
+    matrix: shapes.bullet,
+    offset: {x: x, y: y},
+    direction: {x: 0, y: -1},
+    velocity: 1
   }
 
-  activeBullets.push(bullet)
+  nextElements.push(bullet)
+
   gunEnabled = false
   setTimeout(() => {
     gunEnabled = true
   }, 100)
 }
 
-let gunEnabled = true
-let activeBullets = []
+function createAlien (name, offset) {
+  nextElements.push({
+    type: 3,
+    destroy: false,
+    matrix: shapes[name],
+    offset: offset,
+    direction: {x: 1, y: 0},
+    velocity: 1
+  })
+}
 
-function advanceBullets () {
-  let len = activeBullets.length
-  let deactivate = []
+let gunEnabled = true
+let currentElements = []
+let nextElements = []
+
+
+function advanceElements () {
+  let len = currentElements.length
+
   for (let i = 0; i < len; i++) {
-    let bullet = activeBullets[i]
-    bullet.offset.y -= 1
-    if (arena[bullet.offset.y] && arena[bullet.offset.y][bullet.offset.x] === 0) {
-      drawBullet(bullet)
-    } else {
-      deactivate.push(i)
+    let e = currentElements[i]
+    e.offset.y += e.direction.y * e.velocity
+    e.offset.x += e.direction.x * e.velocity
+
+    collides(e)
+
+    if (!e.destroy) {
+      nextElements.push(e)
     }
-  }
-  for (let i = 0; i < deactivate.length; i++) {
-    activeBullets.splice(i, 1)
   }
 }
 
-
-function drawBullet (bullet) {
-  arena[bullet.offset.y][bullet.offset.x] = 1
+function assembleArena() {
+  addToArena(player)
+  advanceElements()
+  for (let i = 0; i < nextElements.length; i++) {
+    addToArena(nextElements[i])
+  }
 }
 
 // Key handling
 function checkKeys() {
   if (keyStates[37]) {
-    move(player, 'x', -1)
+    move(player, "x", -1)
   }
   if (keyStates[39]) {
-    move(player, 'x', 1)
+    move(player, "x", 1)
   }
   if (keyStates[38]) {
     shoot(player)
@@ -155,20 +223,18 @@ function updateCanvas() {
 
   arena = createArena(83, 117)
   checkKeys()
-  addToArena(player)
-  advanceBullets()
+  assembleArena()
   drawMatrix(arena, {x: 0, y: 0})
+  currentElements = nextElements
+  nextElements = []
   requestAnimationFrame(updateCanvas)
 }
 
-let fighter = {
-  matrix: shapes.fighter,
-  offset: {x: 5, y: 10}
-}
-
 let player = {
+  type: 1,
   matrix: shapes.player,
-  offset: {x: 40, y: 111}
+  offset: {x: 40, y: 111},
+  destroy: false
 }
 
 let arena = createArena(83, 117)
