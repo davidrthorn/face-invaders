185f5e9f19d98e8d07c3b39ea9ae9bb522f1827d
David Rowthorn <davidrowthorn@gmail.com>
2018-09-10 20:34:11 +0100

    Efficient, working game

--- invade.js
+++ invade.js
@@ -2,6 +2,7 @@ const canvas = document.getElementById("invade")
 const context = canvas.getContext("2d")
 
 context.scale(6, 6)
+const arenaSize = {w:83, h:117}
 
 
 const shapes = {
@@ -47,16 +48,20 @@ function createArena (w, h) {
   return arena
 }
 
-function addToArena(element) {
-  element.matrix.forEach((row, y) => {
+function addToArena(e) {
+  [m, o] = [e.matrix, e.offset]
+  if (e.type === 3) {
+    // console.log(o)
+  }
+  m.forEach((row, y) => {
     row.forEach((value, x) => {
-      let target = arena[y + element.offset.y][x + element.offset.x] 
-      if (element.type === 3 && target === 2) {
-        harmBody(element)
-        destroyBullet(y + element.offset.y, x + element.offset.x)
+      let target = arena[y + o.y][x + o.x] 
+      if (e.type === 3 && target === 2) {
+        harmBody(e)
+        destroyBullet(y + o.y, x + o.x)
       }
       if (value !== 0) {
-        arena[y + element.offset.y][x + element.offset.x] = element.matrix[y][x] * element.type
+        arena[y + o.y][x + o.x] = m[y][x] * e.type
       }
     })
   })
@@ -82,53 +87,57 @@ function destroyBullet(y, x){
 
 // Collision logic
 
-function handleCollision (element, axis, target = 1) {
-  if (element.type === 1) return
+function handleCollision (e, axis) {
+  if (e.type === 1) return
 
   if (axis === "x") {
-    if (element.direction.x < 0) {
+    if (e.direction.x < 0) {
       var side = "left"
     } else {
       var side = "right"
     }
   } else {
-    if (element.direction.y < 0) {
+    if (e.direction.y < 0) {
       var side = "bottom"
     } else {
       var side = "top"
     }
   }
 
-  if (element.type === 2) {
-    element.destroy = true
+  if (e.type === 2) {
+    e.destroy = true
   }
 
-  if (element.type === 3) {
+  if (e.type === 3) {
     if (side === "bottom") {
       // Game Over
     } else if (side === "left" || side === "right") {
-      element.direction.x = element.direction.x * -1
+      e.direction.x = e.direction.x * -1
     }
   }
 }
 
-function collides (element) {
-  let [m, o] = [element.matrix, element.offset]
-  for (let y = 0; y < m.length; ++y) {
-    for (let x = 0; x < m[y].length; ++x) {
-      if (!arena[o.y + y]) {
-        handleCollision(element, 'y')
-        return true
-      }
-      if (arena[o.y + y][o.x + x] !== 0) {
-        handleCollision(element, 'x', arena[o.y + y][o.x + x])
-        return true
-      }
+function atEdge(e) {
+  let [m, o] = [e.matrix, e.offset]
+  if (o.x + m[0].length >= arenaSize.w) {
+    o.x -= 1
+    if (e.type === 3) {
+      e.direction.x *= -1
     }
   }
-  return false
+  if (o.x <= 1) {
+    o.x += 1
+    if (e.type === 3) {
+      e.direction.x *= -1
+    }
+  }
+  if (o.y + m.length >= arenaSize.w) {
+    // Logic for the bottom
+  }
+  if (o.y <= 0) {
+    e.destroy = true
+  }
 }
-
 
 function drawMatrix(matrix, offset) {
   matrix.forEach((row, y) => {
@@ -141,12 +150,10 @@ function drawMatrix(matrix, offset) {
   })
 }
 
-function move(element, axis, amount) {
-  let offset = element.offset[axis]
-  element.offset[axis] += amount
-  if (collides(element)) {
-    element.offset[axis] = offset
-  }
+function move(e, axis, amount) {
+  let offset = e.offset[axis]
+  e.offset[axis] += amount
+  atEdge(e)
 }
 
 function shoot(element) {
@@ -177,7 +184,7 @@ function createAlien (name, offset) {
     matrix: shapes[name],
     offset: offset,
     direction: {x: 1, y: 0},
-    velocity: 1,
+    velocity: 2,
     health: 4
   })
 }
@@ -194,10 +201,12 @@ function advanceElements (current, next) {
 
   for (let i = 0; i < len; i++) {
     let e = current[i]
-    e.offset.y += e.direction.y * e.velocity
-    e.offset.x += e.direction.x * e.velocity
+    if (!(frameCount % 2 && e.type === 3)) {
+      e.offset.y += e.direction.y * e.velocity
+      e.offset.x += e.direction.x * e.velocity
+    }
 
-    collides(e)
+    atEdge(e)
 
     if (!e.destroy) {
       next.push(e)
@@ -207,6 +216,7 @@ function advanceElements (current, next) {
 
 function assembleArena() {
   addToArena(player)
+  atEdge(player)
   advanceElements(currentBullets, nextBullets)
   for (let i = 0; i < nextBullets.length; i++) {
     addToArena(nextBullets[i])
@@ -240,10 +250,19 @@ window.onkeyup = e => {
   keyStates[e.keyCode] = false
 }
 
+let frameCount = 0
+
 function updateCanvas() {
+  frameCount += 1
+
+  if (frameCount > 200) {
+    // createAlien('fighter', {x: 40, y: 0})
+    frameCount = 0
+  }
+
   context.fillStyle = "#000"
   context.fillRect(0, 0, canvas.width, canvas.height)
-  arena = createArena(83, 117)
+  arena = createArena(arenaSize.w, arenaSize.h)
   checkKeys()
   assembleArena()
   drawMatrix(arena, {x: 0, y: 0})
@@ -265,5 +284,5 @@ let player = {
   destroy: false
 }
 
-let arena = createArena(83, 117)
+let arena = createArena(arenaSize.w, arenaSize.h)
 updateCanvas()
